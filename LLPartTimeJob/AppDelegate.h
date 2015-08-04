bc5f25b4ec6592f98b018e5c9bf4c9790e4b7c10
luolei <luolei@haodf.com>
2015-08-04 21:49:11 +0800

    Initial Commit

---
+++ LLPartTimeJob/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LLPartTimeJob
+//
+//  Created by Larry on 15/8/4.
+//  Copyright (c) 2015年 Luolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LLPartTimeJob/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLPartTimeJob
+//
+//  Created by Larry on 15/8/4.
+//  Copyright (c) 2015年 Luolei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
