a4e7276ac2651f9d37ddf9eec877864ca180036f
luolei <luolei@haodf.com>
2015-09-06 14:19:34 +0800

    建立目录结构

--- LLPartTimeJob/General/LLRootViewController.m
+++ LLPartTimeJob/General/LLRootViewController.m
@@ -16,13 +16,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"%f, %F",kSCREEN_WIDTH, kSCREEN_HEIGHT);
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 
 @end
